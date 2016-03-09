40f59ba4afed7232ac368f6444bacdae222f1e9b
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-03-09 15:25:19 +0100

    indentation

--- ModularSales/public_html/js/main.js
+++ ModularSales/public_html/js/main.js
@@ -1,6 +1,6 @@
-require(["buyer"], function(buyer) {
-name = 'John Smith';
-console.log(name + " enters the system");
-var result = buyer.buyThing(name);
-console.log("sale success " + result);
+require(["buyer"], function (buyer) {
+    name = 'John Smith';
+    console.log(name + " enters the system");
+    var result = buyer.buyThing(name);
+    console.log("sale success " + result);
 });
